62a1df57075d74438fc5f5cc79983dd2e7c7d21c
Sanskruti Itkar <139566431+Sanskrutiitkar@users.noreply.github.com>
2024-10-30 15:53:52 +0530

    Update StudentService.cs

--- SchoolApi.Business/Services/StudentService.cs
+++ SchoolApi.Business/Services/StudentService.cs
@@ -7,37 +7,7 @@ namespace SchoolApi.Business.Services
 {
     public class StudentService : IStudentService
     {
-        private readonly IStudentRepo _studentRepo;
-
-        public StudentService(IStudentRepo studentRepo)
-        {
-            _studentRepo = studentRepo;
-        }
-
-        // public async Task<Student> AddStudent(Student student)
-        // {
-        //     return await _studentRepo.AddStudent(student);
-        // }
-
-        // public async Task DeleteStudent(int studentID)
-        // {
-        //      await _studentRepo.DeleteStudent(studentID);
-        // }
-
-        // public async Task<IEnumerable<Student>> GetAllStudents()
-        // {
-        //     return await _studentRepo.GetAllStudents();
-        // }
-
-        // public async Task<PagedResponse<Student>> GetSearchedStudents(string search, int pageNumber, int pageSize)
-        // {
-        //     return await _studentRepo.GetSearchedStudents(search, pageNumber, pageSize);
-        // }
-
-        // public async Task UpdateStudent(int id, Student student)
-        // {
-        //      await _studentRepo.UpdateStudent(id, student);
-        // }
+        
         public int CalculateAge(DateTime birthDate)
         {
             DateTime today = DateTime.Today;
